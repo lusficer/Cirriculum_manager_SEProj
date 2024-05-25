30cde121e425990ae801f05e78d253408b219cc1
Huy Hoang <bachhuy410@gmail.com>
2024-05-25 20:50:56 +0700

    fixing

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  css: {
-    postcss: '../frontend/postcss.config.js',
-  },
+	plugins: [react()],
+	css: {
+		postcss: './postcss.config.js',
+	},
 })
